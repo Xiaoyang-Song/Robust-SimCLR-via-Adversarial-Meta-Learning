432d5c5288319f8882e1e5bdf5bd74ae583c405a
IrmaDong <irmadong@irmadongs-mbp.myfiosgateway.com>
2022-11-13 19:54:48 -0500

    updated test cases for attack

--- attack/attack.py
+++ attack/attack.py
@@ -28,6 +28,7 @@ class FGSMAttack(nn.Module):
     def __init__(self, model, linear,original_images, labels, epsilon, alpha, min_val, max_val, max_iters, _type='linf', reduction4loss='mean', random_start=True):
 
         # Model
+        super(FGSMAttack, self).__init__()
         self.model = model
         self.linear = linear
         # Maximum perturbation
@@ -42,6 +43,10 @@ class FGSMAttack(nn.Module):
         self.max_iters = max_iters
         # The perturbation of epsilon
         self._type = _type
+        self.reduction4loss = reduction4loss
+        self.random_start = random_start
+        self.original_images = original_images
+        self.labels = labels
 
     def perturb(self):
         # original_images: values are within self.min_val and self.max_val
@@ -50,7 +55,7 @@ class FGSMAttack(nn.Module):
         if self.random_start:
             rand_perturb = torch.FloatTensor(self.original_images.shape).uniform_(
                 -self.epsilon, self.epsilon)
-            rand_perturb = rand_perturb.cuda()
+            #rand_perturb = rand_perturb.cuda()
             x = self.original_images.clone() + rand_perturb
             x = torch.clamp(x, self.min_val, self.max_val)
         else:
@@ -73,7 +78,7 @@ class FGSMAttack(nn.Module):
         else:
             outputs = self.linear(self.model(x))
 
-        loss = F.cross_entropy(outputs, self.labels, reduction=self.reduction4loss)
+        loss = F.mse_loss(outputs, self.model(self.labels))
 
         grad_outputs = None
         grads = torch.autograd.grad(loss, x, grad_outputs=grad_outputs, only_inputs=True, retain_graph=False)[0]
@@ -84,31 +89,30 @@ class FGSMAttack(nn.Module):
         x.data += self.alpha * scaled_g
 
         x = torch.clamp(x, self.min_val, self.max_val)
-        x = project(x, self.original_images, self.epsilon, self._type)
+        #x = project(x, self.original_images, self.epsilon, self._type)
         #not sure why here and just check the loss
         self.model.train()
-        self.projector.train()
-        self.optimizer.zero_grad()
+        #self.projector.train()
+        #self.optimizer.zero_grad()
         if self.linear == 'None':
             outputs = self.model(x)
         else:
             outputs = self.linear(self.model(x))
-        loss = F.cross_entropy(outputs, self.labels, reduction=self.reduction4loss)
+        #loss = F.cross_entropy(outputs, self.labels, reduction=self.reduction4loss)
+        loss = F.mse_loss(outputs, self.model(self.labels))
 
 
         return x.detach(), loss
-
-
 class PGDAttack(nn.Module):
     def __init__(self, model, ori, target,  projector, epsilon, alpha, min_val, max_val,
                  max_iters,optimizer, batch_size, temperature,
-                 random_start = True, _type='linf', loss_type='sim', regularize='original'):
+                 random_start = True, _type='linf', loss_type='sim'):
         # Constructor for
+        super(PGDAttack, self).__init__()
         self.model = model
         self.ori = ori
         self.target = target
         self.projector = projector
-        self.regularize = regularize
         # Maximum perturbation
         self.epsilon = epsilon
         # Movement multiplier per iteration
@@ -130,13 +134,15 @@ class PGDAttack(nn.Module):
         self.temperature = temperature
         self.random_start = random_start
 
-    def get_loss(self):
+    def perturb(self):
         if self.random_start:
             rand_perturb = torch.FloatTensor(self.ori.shape).uniform_(
                 -self.epsilon, self.epsilon)
-            rand_perturb = rand_perturb.float().cuda()
+            if torch.cuda.is_available():
+                rand_perturb = rand_perturb.float().cuda()
             x = self.ori.float().clone() + rand_perturb
             x = torch.clamp(x, self.min_val, self.max_val)
+            print("shape of x", x.shape)
         else:
             x = self.ori.clone()
 
@@ -145,7 +151,7 @@ class PGDAttack(nn.Module):
         self.model.eval()
         self.projector.eval()
         batch_size = self.batch_size
-
+        criteria = RBSimCLRLoss(self.temperature, self. batch_size)
         with torch.enable_grad():
             for _iter in range(self.max_iters):
 
@@ -153,19 +159,19 @@ class PGDAttack(nn.Module):
                 self.projector.zero_grad()
 
                 if self.loss_type == 'mse':
-                    loss = F.mse_loss(self.projector(self.model(x)), self.projector(self.model(self.target)))
+                    loss = F.mse_loss(self.model(x), self.model(self.target))
                 elif self.loss_type == 'sim':
                     #inputs = torch.cat((x, self.target))
                     #output = self.projector(self.model(inputs))
-                    loss = RBSimCLRLoss(self.temperature, self. batch_size )
+                    loss = criteria.forward(x, self.ori)
+
 
                 elif self.loss_type == 'l1':
-                    loss = F.l1_loss(self.projector(self.model(x)), self.projector(self.model(self.target)))
-                elif self.loss_type == 'l2':
-                    loss = F.l2_loss(self.projector(self.model(x)), self.projector(self.model(self.target)))
+                    loss = F.l1_loss(self.model(x), self.model(self.target))
+
                 elif self.loss_type == 'cos':
-                    loss = 1 - F.cosine_similarity(self.projector(self.model(x)),
-                                                   self.projector(self.model(self.target))).mean()
+                    loss = 1 - F.cosine_similarity(self.model(x),
+                                                   self.model(self.target)).mean()
 
                 grads = torch.autograd.grad(loss, x, grad_outputs=None, only_inputs=True, retain_graph=False)[0]
 
@@ -178,11 +184,11 @@ class PGDAttack(nn.Module):
                 x = project(x, self.ori, self.epsilon, self._type)
 
         self.model.train()
-        self.projector.train()
+        #self.projector.train()
         self.optimizer.zero_grad()
 
         if self.loss_type == 'mse':
-            loss = F.mse_loss(self.projector(self.model(x)), self.projector(self.model(self.target))) * (1.0 / batch_size)
+            loss = F.mse_loss(self.model(x), self.model(self.target)) * (1.0 / batch_size)
         elif self.loss_type == 'sim':
 
             #inputs = torch.cat((x, self.target))
@@ -190,12 +196,114 @@ class PGDAttack(nn.Module):
             #similarity, _ = pairwise_similarity(output, temperature=0.5, multi_gpu=False, adv_type='None')
             loss = RBSimCLRLoss(self.temperature, self. batch_size )
         elif self.loss_type == 'l1':
-            loss = F.l1_loss(self.projector(self.model(x)), self.projector(self.model(self.target))) * (1.0 / batch_size)
+            loss = F.l1_loss(self.model(x), self.model(self.target)) * (1.0 / batch_size)
         elif self.loss_type == 'l2':
-            loss = F.l2_loss(self.projector(self.model(x)), self.projector(self.model(self.target)))* (1.0 / batch_size)
+            loss = F.l2_loss(self.model(x), self.model(self.target))* (1.0 / batch_size)
         elif self.loss_type == 'cos':
-            loss = 1 - F.cosine_similarity(self.projector(self.model(x)), self.projector(self.model(self.target))).sum() * (
+            loss = 1 - F.cosine_similarity(self.model(x), self.model(self.target)).sum() * (
                         1.0 / batch_size)
 
         return x.detach(), loss
+
+
+# class PGDAttack(nn.Module):
+#     def __init__(self, model, ori, target,  projector, epsilon, alpha, min_val, max_val,
+#                  max_iters,optimizer, batch_size, temperature,
+#                  random_start = True, _type='linf', loss_type='sim'):
+#         # Constructor for
+#         super(PGDAttack, self).__init__()
+#         self.model = model
+#         self.ori = ori
+#         self.target = target
+#         self.projector = projector
+#         # Maximum perturbation
+#         self.epsilon = epsilon
+#         # Movement multiplier per iteration
+#         self.alpha = alpha
+#         # Minimum value of the pixels
+#         self.min_val = min_val
+#         # Maximum value of the pixels
+#         self.max_val = max_val
+#         # Maximum numbers of iteration to generated adversaries
+#         self.max_iters = max_iters
+#         # The perturbation of epsilon
+#         #Todo: Jing l2 distance
+#         self._type = _type
+#         # loss type
+#         self.loss_type = loss_type
+#         self.type_attack = TypeAttack.PGD
+#         self.optimizer = optimizer
+#         self.batch_size =batch_size
+#         self.temperature = temperature
+#         self.random_start = random_start
+#
+#     def perturb(self):
+#         if self.random_start:
+#             rand_perturb = torch.FloatTensor(self.ori.shape).uniform_(
+#                 -self.epsilon, self.epsilon)
+#             if torch.cuda.is_available():
+#                 rand_perturb = rand_perturb.float().cuda()
+#             x = self.ori.float().clone() + rand_perturb
+#             x = torch.clamp(x, self.min_val, self.max_val)
+#         else:
+#             x = self.ori.clone()
+#
+#         x.requires_grad = True
+#
+#         self.model.eval()
+#         self.projector.eval()
+#         batch_size = self.batch_size
+#
+#         with torch.enable_grad():
+#             for _iter in range(self.max_iters):
+#
+#                 self.model.zero_grad()
+#                 self.projector.zero_grad()
+#
+#                 if self.loss_type == 'mse':
+#                     loss = F.mse_loss(self.projector(self.model(x)), self.projector(self.model(self.target)))
+#                 elif self.loss_type == 'sim':
+#                     #inputs = torch.cat((x, self.target))
+#                     #output = self.projector(self.model(inputs))
+#                     loss = RBSimCLRLoss(self.temperature, self. batch_size )
+#
+#                 elif self.loss_type == 'l1':
+#                     loss = F.l1_loss(self.projector(self.model(x)), self.projector(self.model(self.target)))
+#                 elif self.loss_type == 'l2':
+#                     loss = F.l2_loss(self.projector(self.model(x)), self.projector(self.model(self.target)))
+#                 elif self.loss_type == 'cos':
+#                     loss = 1 - F.cosine_similarity(self.projector(self.model(x)),
+#                                                    self.projector(self.model(self.target))).mean()
+#
+#                 grads = torch.autograd.grad(loss, x, grad_outputs=None, only_inputs=True, retain_graph=False)[0]
+#
+#                 if self._type == 'linf':
+#                     scaled_g = torch.sign(grads.data)
+#
+#                 x.data += self.alpha * scaled_g
+#
+#                 x = torch.clamp(x, self.min_val, self.max_val)
+#                 x = project(x, self.ori, self.epsilon, self._type)
+#
+#         self.model.train()
+#         self.projector.train()
+#         self.optimizer.zero_grad()
+#
+#         if self.loss_type == 'mse':
+#             loss = F.mse_loss(self.projector(self.model(x)), self.projector(self.model(self.target))) * (1.0 / batch_size)
+#         elif self.loss_type == 'sim':
+#
+#             #inputs = torch.cat((x, self.target))
+#             #output = self.projector(self.model(inputs))
+#             #similarity, _ = pairwise_similarity(output, temperature=0.5, multi_gpu=False, adv_type='None')
+#             loss = RBSimCLRLoss(self.temperature, self. batch_size )
+#         elif self.loss_type == 'l1':
+#             loss = F.l1_loss(self.projector(self.model(x)), self.projector(self.model(self.target))) * (1.0 / batch_size)
+#         elif self.loss_type == 'l2':
+#             loss = F.l2_loss(self.projector(self.model(x)), self.projector(self.model(self.target)))* (1.0 / batch_size)
+#         elif self.loss_type == 'cos':
+#             loss = 1 - F.cosine_similarity(self.projector(self.model(x)), self.projector(self.model(self.target))).sum() * (
+#                         1.0 / batch_size)
+#
+#         return x.detach(), loss
 
---
+++ attack/test_attack.py
@@ -0,0 +1,170 @@
+from data.dataset import CIFAR10
+from models.resnet import get_resnet
+from data.dataset import ContrastiveLearningDataset
+import models
+import torch
+import matplotlib.pyplot as plt
+import numpy as np
+import torch.backends.cudnn as cudnn
+import torch.nn as nn
+import torch.optim as optim
+from icecream import ic
+from models.projector import Projector
+from collections import OrderedDict
+from attack import PGDAttack, FGSMAttack
+from PIL import Image
+import PIL
+
+import data
+use_cuda = torch.cuda.is_available()
+if use_cuda:
+    ngpus_per_node = torch.cuda.device_count()
+best_acc = 0  # best test accuracy
+start_epoch = 0  # start from epoch 0 or last checkpoint epoch
+
+model= get_resnet("resnet50", True)
+Linear = nn.Sequential(nn.Linear(512 , 10))
+# train_dataset, val_dataset, train_loader, val_loader = CIFAR10(2,2)
+
+dataset = ContrastiveLearningDataset("./datasets")
+num_views = 2
+train_dataset = dataset.get_dataset('cifar10_tri', num_views)
+# train_loader = torch.utils.data.DataLoader(
+#         train_dataset, batch_size=2, shuffle=True)
+
+img_view = torch.stack(train_dataset[0][0])
+
+criterion = nn.CrossEntropyLoss()
+use_cuda = torch.cuda.is_available()
+if use_cuda:
+    ngpus_per_node = torch.cuda.device_count()
+    model.cuda()
+    Linear.cuda()
+    cudnn.benchmark = True
+
+#attacker = PGDAttack(model, Linear, epsilon=0.0314, alpha=0.007, min_val=0.0, max_val=1.0, max_iters=10, _type="linf")
+def test_singleimgpgd(img,idx, losstype = "mse"):
+
+    base_optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.9)
+    projector = Projector(1)
+    name = "pgd"
+    attacker = PGDAttack(model=model, ori=img, target=img, projector=projector, epsilon=0.0314, alpha=0.007,
+                         min_val=0.0, max_val=1.0, max_iters=10, optimizer=base_optimizer, batch_size=2,
+                         temperature=0.5, loss_type=losstype
+                         )
+
+    adv_inputs, _ = attacker.perturb()
+    plt.imshow(np.array(img[idx].reshape(224, 224, 3)))
+    fn1 = name + losstype + str(idx) + ".png"
+    plt.savefig(fn1)
+
+    #plt.show()
+    plt.imshow(np.array(adv_inputs[idx].reshape(224, 224, 3)))
+    fn2 = name + losstype+"_adv" + str(idx) + ".png"
+    plt.savefig(fn2)
+    #plt.show()
+
+
+def test_singleimgfgsm(img,idx):
+
+    base_optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.9)
+
+    name = "pgd"
+    attacker = FGSMAttack(model=model, linear = 'None', original_images=img, labels=img,  epsilon=0.0314, alpha=0.007,
+                         min_val=0.0, max_val=1.0, max_iters=10)
+
+    # def __init__(self, model, linear, original_images, labels, epsilon, alpha, min_val, max_val, max_iters,
+    #              _type='linf', reduction4loss='mean', random_start=True):
+
+    name = "fgsm"
+    adv_inputs, _ = attacker.perturb()
+    plt.imshow(np.array(img[idx].reshape(224, 224, 3)))
+    fn1 = name  + str(idx) + ".png"
+    plt.savefig(fn1)
+
+    #plt.show()
+    plt.imshow(np.array(adv_inputs[idx].reshape(224, 224, 3)))
+    fn2 = name +"_adv" + str(idx) + ".png"
+    plt.savefig(fn2)
+    #plt.show()
+
+
+# test_singleimgpgd("pgd", img_view,0, "mse")
+# test_singleimgpgd("pgd", img_view,0, "l1")
+# test_singleimgpgd("pgd", img_view,0, "cos")
+
+
+#test_singleimgfgsm(img_view, 0)
+test_singleimgpgd(img_view,0, "sim")
+# def test(name, losstype="mse" ):
+#     global best_acc
+#
+#     model.eval()
+#     Linear.eval()
+#
+#     test_clean_loss = 0
+#     test_adv_loss = 0
+#     clean_correct = 0
+#     adv_correct = 0
+#     clean_acc = 0
+#     total = 0
+#     base_optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.9)
+#
+#     for idx, (image, label) in enumerate(val_loader):
+#
+#
+#         img = image
+#         y = label
+#         print(y)
+#         print(img.shape)
+#         total += y.size(0)
+#         # ic(img.shape)
+#         # ic(model(img).shape)
+#         # out = Linear(model(img))
+#         # _, predx = torch.max(out.data, 1)
+#         # clean_loss = criterion(out, y)
+#         #
+#         # clean_correct += predx.eq(y.data).cpu().sum().item()
+#         #
+#         # clean_acc = 100. * clean_correct / total
+#         #
+#         # test_clean_loss += clean_loss.data
+#         projector = Projector(1)
+#
+#         if name == "pgd":
+#             attacker = PGDAttack(model = model, ori = image, target = image, projector = projector, epsilon=0.0314, alpha=0.007,
+#                                  min_val=0.0, max_val=1.0, max_iters=10,optimizer= base_optimizer, batch_size= 2,
+#                                  temperature=0.5, loss_type = losstype
+#                                 )
+#
+#         # self, model, ori, target, projector, epsilon, alpha, min_val, max_val,
+#         # max_iters, optimizer, batch_size, temperature,
+#         # random_start = True, _type = 'linf', loss_type = 'sim', regularize = 'original'
+#         adv_inputs, _= attacker.perturb()
+#         print("shape of adv_Input", adv_inputs.shape)
+#         out = model(adv_inputs)
+#         # _, index = torch.max(out, 1)
+#         # percentage = torch.nn.functional.softmax(out, dim=1)[0] * 100
+#         print("out in the followinf ")
+#         print(out.shape) #todo why 2*1000? cuz pretrained
+#         # print(percentage)
+#         # print(y[index[0]], percentage[index[0]].item())
+#         #print(out.shape)
+#         #ic(len(out))
+#         #print(adv_inputs.shape)
+#         # plt.imshow(np.array(cifar_tri[0][0].permute(1, 2, 0)))
+#         _, predx = torch.max(out.data, 1)
+#         adv_loss = criterion(out, y)
+#
+#         adv_correct += predx.eq(y.data).cpu().sum().item()
+#         adv_acc = 100. * adv_correct / total
+#
+#         test_adv_loss += adv_loss.data
+#         print('hi')
+#         break
+#
+#
+#     print("Test accuracy: {0}/{1}".format(clean_acc, adv_acc))
+#
+#     return (clean_acc, adv_acc)
+
