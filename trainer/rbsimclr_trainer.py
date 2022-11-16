4f7ba004864d7f140812c8ffcb8d0a9c8546612e
IrmaDong <irmadong@irmadongs-mbp.myfiosgateway.com>
2022-11-15 23:36:27 -0500

    debug

--- trainer/rbsimclr_trainer.py
+++ trainer/rbsimclr_trainer.py
@@ -138,7 +138,7 @@ if __name__ == '__main__':
     train_dataset = dataset.get_dataset('cifar10_tri', num_views)
     val_dataset = dataset.get_dataset('cifar10_val', num_views)
     # Batch size config
-    bsz_tri, bsz_val = 256, 128
+    bsz_tri, bsz_val = 64, 64
     train_loader = torch.utils.data.DataLoader(
         train_dataset, batch_size=bsz_tri, shuffle=True,
         num_workers=2, pin_memory=True, drop_last=True)
